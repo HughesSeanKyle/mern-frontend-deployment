bcecd3a40477b4c002a6101e51fb70a8ac63032d
HughesSeanKyle <khughessean@yahoo.com>
2022-08-23 16:39:51 +0200

    Convert register POST req  fetch to axios

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
 	REGISTER_SUCCESS,
 	REGISTER_FAIL,
@@ -5,39 +6,76 @@ import {
 	AUTH_ERROR,
 } from './types';
 
-// Register User
-export const signUp = ({ username, email, password }) => async (dispatch) => {
-	const myHeaders = new Headers();
-	myHeaders.append('Content-Type', 'application/json');
+// Load User
+// export const loadUser = () => async (dispatch) => {};
 
-	const raw = JSON.stringify({
+// Register User fetch
+// export const signUp = ({ username, email, password }) => async (dispatch) => {
+// 	const myHeaders = new Headers();
+// 	myHeaders.append('Content-Type', 'application/json');
+
+// 	const raw = JSON.stringify({
+// 		name: username,
+// 		email: email,
+// 		password: password,
+// 	});
+
+// 	const requestOptions = {
+// 		method: 'POST',
+// 		headers: myHeaders,
+// 		body: raw,
+// 		redirect: 'follow',
+// 	};
+
+// 	fetch('https://aqueous-retreat-11852.herokuapp.com/api/user', requestOptions)
+// 		.then((response) => response.json())
+// 		.then((result) => {
+// 			// Note token to be received here and must be stored to state
+// 			// result must be returned
+// 			// When status 200 okay
+// 			console.log(result);
+// 			dispatch({
+// 				type: REGISTER_SUCCESS,
+// 				payload: result,
+// 			});
+// 		})
+// 		.catch((error) => {
+// 			dispatch({
+// 				type: REGISTER_FAIL,
+// 			});
+// 		});
+// };
+
+// Register a user axios
+export const signUp = ({ username, email, password }) => async (dispatch) => {
+	const config = {
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	};
+
+	const body = JSON.stringify({
 		name: username,
 		email: email,
 		password: password,
 	});
 
-	const requestOptions = {
-		method: 'POST',
-		headers: myHeaders,
-		body: raw,
-		redirect: 'follow',
-	};
+	try {
+		const res = await axios.post(
+			'https://aqueous-retreat-11852.herokuapp.com/api/user',
+			body,
+			config
+		);
 
-	fetch('https://aqueous-retreat-11852.herokuapp.com/api/user', requestOptions)
-		.then((response) => response.json())
-		.then((result) => {
-			// Note token to be received here and must be stored to state
-			// result must be returned
-			// When status 200 okay
-			console.log(result);
-			dispatch({
-				type: REGISTER_SUCCESS,
-				payload: result,
-			});
-		})
-		.catch((error) => {
-			dispatch({
-				type: REGISTER_FAIL,
-			});
+		dispatch({
+			type: REGISTER_SUCCESS,
+			payload: res.data,
 		});
+
+		//   dispatch(loadUser());
+	} catch (err) {
+		dispatch({
+			type: REGISTER_FAIL,
+		});
+	}
 };
